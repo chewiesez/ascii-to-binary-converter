ac179efae5e118ea2e0abd8566f7bb5676d21025
Dan Foster <60412752+chewiesez@users.noreply.github.com>
2024-02-02 22:04:21 -0800

    Update Binary_Converter.py
    changed some wording to more accurately describe the function.

--- Binary_Converter.py
+++ Binary_Converter.py
@@ -4,10 +4,10 @@
 Created on Fri Oct 21 11:39:28 2022
 
 @author: fost
-python challenge 3 convert a decimal to binary 
+convert ascii to binary 
 """
 def binary_convert():
-    d = str(input('Enter a decimal number to convert:'))
+    d = input('Enter any ASCII/Unicode text to convert to binary:')
     c = ' '.join('{0:08b}'.format(ord(x), 'b') for x in d)
            
     print(c)
